d6e95219c8cd9b3f271060b7e7ed29f6eaa8c935
Kier Davis <kierdavis@gmail.com>
2017-02-22 17:58:43 +0000

    Power and motor boards use different versions of libopencm3, so move existing libopencm3 into power/

--- default.nix
+++ default.nix
@@ -2,8 +2,7 @@ let
   pkgs = import <nixpkgs> {};
 in
   rec {
-    libopencm3 = pkgs.callPackage ./libopencm3 {};
-
-    power.firmware = pkgs.callPackage ./power/firmware { libopencm3 = libopencm3; };
+    power.libopencm3 = pkgs.callPackage ./power/libopencm3 {};
+    power.firmware = pkgs.callPackage ./power/firmware { libopencm3 = power.libopencm3; };
     power.flash = pkgs.callPackage ./power/flash { powerFirmware = power.firmware; };
   }
