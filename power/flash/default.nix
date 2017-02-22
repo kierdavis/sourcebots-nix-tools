27b6272270d973eff6e0c76c08a33ca8bcdaa70b
Kier Davis <kierdavis@gmail.com>
2017-02-22 17:55:37 +0000

    Add task for flashing power board over UART using stm32flash

--- power/flash/default.nix
+++ power/flash/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, powerFirmware, openocd, writeScriptBin }:
+{ stdenv, powerFirmware, openocd, stm32flash, writeScriptBin }:
 
 let
   flash = name: image: writeScriptBin name ''
@@ -15,4 +15,13 @@ in
   {
     fw = flash "pbv4-flash-fw" "${powerFirmware}/images/pbv4.elf";
     testfw = flash "pbv4-flash-testfw" "${powerFirmware}/images/pbv4_test.elf";
+
+    testuart = writeScriptBin "pbv4-flash-testuart" ''
+      #!${stdenv.shell}
+      if [ -z "$1" ]; then
+        echo "usage: $0 /dev/ttyUSB0"
+        exit 2
+      fi
+      ${stm32flash}/bin/stm32flash -b 115200 -w ${powerFirmware}/images/pbv4_test.bin "$1"
+    '';
   }
