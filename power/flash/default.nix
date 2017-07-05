e3b4f8372ae1e334b89d0d8b14a31ed6fea3d11d
Kier Davis <kierdavis@gmail.com>
2017-07-05 21:35:59 +0100

    Rework power board flashing scripts

--- power/flash/default.nix
+++ power/flash/default.nix
@@ -1,27 +1,21 @@
-{ stdenv, powerFirmware, openocd, stm32flash, writeScriptBin }:
+{ stdenv, powerFirmware, openocd, stm32flash }:
 
-let
-  flash = name: image: writeScriptBin name ''
-    #!${stdenv.shell}
-    ${openocd}/bin/openocd -f ${powerFirmware}/oocd/pbv4.cfg \
-      -c "init" \
-      -c "reset init" \
-      -c "stm32f1x mass_erase 0" \
-      -c "flash write_image ${image}" \
-      -c "reset" \
-      -c "shutdown"
+stdenv.mkDerivation {
+  inherit powerFirmware openocd stm32flash;
+  inherit (stdenv) shell;
+  name = "pbv4-flash";
+  src = ./.;
+  buildPhase = ''
+    mkdir -p bin
+    imagename=pbv4.elf      substituteAll src/pbv4-flash-oocd.template bin/pbv4-flash-fw-oocd
+    imagename=pbv4_test.elf substituteAll src/pbv4-flash-oocd.template bin/pbv4-flash-testfw-oocd
+    imagename=pbv4.bin      substituteAll src/pbv4-flash-uart.template bin/pbv4-flash-fw-uart
+    imagename=pbv4_test.bin substituteAll src/pbv4-flash-uart.template bin/pbv4-flash-testfw-uart
   '';
-in
-  {
-    fw = flash "pbv4-flash-fw" "${powerFirmware}/images/pbv4.elf";
-    testfw = flash "pbv4-flash-testfw" "${powerFirmware}/images/pbv4_test.elf";
-
-    testuart = writeScriptBin "pbv4-flash-testuart" ''
-      #!${stdenv.shell}
-      if [ -z "$1" ]; then
-        echo "usage: $0 /dev/ttyUSB0"
-        exit 2
-      fi
-      ${stm32flash}/bin/stm32flash -b 115200 -w ${powerFirmware}/images/pbv4_test.bin "$1"
-    '';
-  }
+  installPhase = ''
+    for src in bin/*; do
+      dest=$out/bin/$(basename $src)
+      install -D -m 0755 $src $dest
+    done
+  '';
+}
