7cb2d93e5b8a87458a00c5ab4bae29ca0e743f79
Kier Davis <kierdavis@gmail.com>
2017-07-05 21:36:30 +0100

    Add an expression for python-libusb1

--- default.nix
+++ default.nix
@@ -2,6 +2,8 @@ let
   pkgs = import <nixpkgs> {};
 in
   rec {
+    lib.python-libusb1 = pkgs.callPackage ./lib/python-libusb1 {};
+
     power.libopencm3 = pkgs.callPackage ./power/libopencm3 {};
     power.firmware = pkgs.callPackage ./power/firmware { libopencm3 = power.libopencm3; };
     power.flash = pkgs.callPackage ./power/flash { powerFirmware = power.firmware; };
