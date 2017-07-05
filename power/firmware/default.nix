ccc39a8b24fadfb64193c7da509c2903c0d5c55d
Kier Davis <kierdavis@gmail.com>
2017-07-05 21:35:04 +0100

    Bump revision of power-v4-fw
    This includes a commit which fixes the path to the cable config file for the OOCD flashing scripts.

--- power/firmware/default.nix
+++ power/firmware/default.nix
@@ -6,8 +6,8 @@ stdenv.mkDerivation {
     owner = "sourcebots";
     repo = "power-v4-fw";
     fetchSubmodules = true;
-    rev = "550b95e5dbf66cf962853a9cbbd26b88a8cc66a2";
-    sha256 = "0wvi8pcm943xpcl0yf03ppb89wb9fyv6wgjzhkms6g9f6xi7h8dc";
+    rev = "c609c89d2b70fa1a3ff781a02eb10942157964eb";
+    sha256 = "06svznibm8vklhwgv3ivc8z932j7c6p2cd83p3fl8mlaa3rhkjc9";
   };
   buildInputs = [ boost gcc-arm-embedded libopencm3 ];
   LIBOPENCM3 = "${libopencm3}/arm-none-eabi";
