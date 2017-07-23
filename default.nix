8f9b7d474b1c3e95c006438e6538cd37978fa4db
Kier Davis <kierdavis@gmail.com>
2017-07-23 22:27:54 +0100

    Refactor: top-level

--- default.nix
+++ default.nix
@@ -2,13 +2,13 @@ let
   pkgs = import <nixpkgs> {};
 in
   rec {
-    power.libopencm3 = pkgs.callPackage ./boards/pbv4/libopencm3.nix {};
-    power.firmware = pkgs.callPackage ./boards/pbv4/firmware.nix { pbv4-libopencm3 = power.libopencm3; };
-    power.flash = pkgs.callPackage ./boards/pbv4/flash-scripts.nix { pbv4-firmware = power.firmware; };
+    pbv4-libopencm3 = pkgs.callPackage ./boards/pbv4/libopencm3.nix {};
+    pbv4-firmware = pkgs.callPackage ./boards/pbv4/firmware.nix { inherit pbv4-libopencm3; };
+    pbv4-flash-scripts = pkgs.callPackage ./boards/pbv4/flash-scripts.nix { inherit pbv4-firmware; };
 
-    motor.libopencm3 = pkgs.callPackage ./boards/mcv4/libopencm3.nix {};
-    motor.firmware = pkgs.callPackage ./boards/mcv4/firmware.nix { mcv4-libopencm3 = motor.libopencm3; };
-    motor.flash = pkgs.callPackage ./motor/flash { motorFirmware = motor.firmware; };
+    mcv4-libopencm3 = pkgs.callPackage ./boards/mcv4/libopencm3.nix {};
+    mcv4-firmware = pkgs.callPackage ./boards/mcv4/firmware.nix { inherit mcv4-libopencm3; };
+    mcv4-flash-scripts = pkgs.callPackage ./motor/flash { motorFirmware = mcv4-firmware; };
 
-    sw.robotd = pkgs.callPackage ./sw/robotd {};
+    robotd = pkgs.callPackage ./sw/robotd {};
   }
