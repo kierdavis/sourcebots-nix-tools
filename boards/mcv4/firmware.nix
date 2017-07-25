8791a8834f00b77558612c3aa25cea8de553a0d6
Kier Davis <kierdavis@gmail.com>
2017-07-25 20:29:13 +0100

    Add an mcv4-firmware-dir script to the mcv4 firmware derivation, same as for the pbv4

--- boards/mcv4/firmware.nix
+++ boards/mcv4/firmware.nix
@@ -14,6 +14,13 @@ stdenv.mkDerivation {
   installPhase = ''
     install -D -m 0644 -t $out/images mcv4.{bin,elf} mcv4_test.{bin,elf}
     install -D -m 0644 -t $out/oocd oocd/mcv4.cfg
+
+    mkdir -p $out/bin
+    cat >$out/bin/mcv4-firmware-dir <<EOF
+      #!${stdenv.shell}
+      echo $out/images
+    EOF
+    chmod +x $out/bin/mcv4-firmware-dir
   '';
 
   meta = with stdenv.lib; {
