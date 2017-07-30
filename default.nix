39957faaaf89cc7fd7b60e1127bd0ad9aeb79c20
Kier Davis <kierdavis@gmail.com>
2017-07-30 09:27:42 +0100

    Remove now-redundant pbv4-flash-scripts and mcv4-flash-scripts derivations

--- default.nix
+++ default.nix
@@ -4,11 +4,9 @@ in
   rec {
     pbv4-libopencm3 = pkgs.callPackage ./boards/pbv4/libopencm3.nix {};
     pbv4-firmware = pkgs.callPackage ./boards/pbv4/firmware.nix { inherit pbv4-libopencm3; };
-    pbv4-flash-scripts = pkgs.callPackage ./boards/pbv4/flash-scripts.nix { };
 
     mcv4-libopencm3 = pkgs.callPackage ./boards/mcv4/libopencm3.nix {};
     mcv4-firmware = pkgs.callPackage ./boards/mcv4/firmware.nix { inherit mcv4-libopencm3; };
-    mcv4-flash-scripts = pkgs.callPackage ./motor/flash { motorFirmware = mcv4-firmware; };
 
     tools = pkgs.callPackage ./tools.nix {};
     sb-vision = pkgs.callPackage ./sw/sb-vision.nix {};
