6057066137524664f09dcd2041d14e19b6cbeda9
Kier Davis <kierdavis@gmail.com>
2017-11-05 17:14:18 +0000

    Move SB tools environment into tools/

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ in
     mcv4-libopencm3 = pkgs.callPackage ./boards/mcv4/libopencm3.nix {};
     mcv4-firmware = pkgs.callPackage ./boards/mcv4/firmware.nix { inherit mcv4-libopencm3; };
 
-    tools = pkgs.callPackage ./tools.nix {};
+    sb-tools = pkgs.callPackage ./tools/sb-tools.nix {};
     sb-vision = pkgs.callPackage ./sw/sb-vision.nix {};
     robotd = pkgs.callPackage ./sw/robotd.nix { inherit sb-vision; };
     robot-api = pkgs.callPackage ./sw/robot-api {};
